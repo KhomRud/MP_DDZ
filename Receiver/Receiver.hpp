9293cb506fd70da4f53a0523290c39fda3bebbd2
alexvf6 <alexvf6@mail.ru>
2017-11-06 22:38:12 -0800

    Проверка

---
+++ Receiver/Receiver.hpp
@@ -0,0 +1,17 @@
+
+
+#ifndef RECEIVER_HPP
+#define RECEIVER_HPP
+
+
+
+#endif /* RECEIVER_HPP */
+
+class Receiver
+{
+    public:
+        Receiver();
+        ~Receiver();
+        
+    private:
+}

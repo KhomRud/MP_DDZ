3570a11c4f36c2b2e2c228ab13363c6e96c8908b
Ivandemian <cr0pssp0t@gmail.com>
2017-11-21 12:21:33 -0800

    Добавил метод удаления из кэша данных с просроченным временем. Осталось отрефакторить и прокомментировать.

--- ProxyServer.Modules/Cacher/Cacher.h
+++ ProxyServer.Modules/Cacher/Cacher.h
@@ -1,196 +1,60 @@
-#ifndef CACHER_H
-#define CACHER_H
-
-#define casherFolder "./CachingData.txt"
-
 #include <vector>
-#include <time.h>
+#include <sstream>
+#include <iostream>
 #include <fstream>
-#include "regex.h"
+#include <stdlib.h>
+#include <stdexcept>
+#include <ctime>
+#include <stdio.h>
 
 using namespace std;
 
-/*
-
-struct cacheInfo
+struct CacheInfo
 {
-    string _request;
-    string _time;
-    string _answer;              
+    string Request;
+    string Answer;
+    int    Time;	
 };
 
-class Cacher
+template <typename T>
+string toString(T val)
 {
-    
-private:
-    cacheInfo _cacheData;
-    
+    ostringstream oss;
+    oss << val;
+    return oss.str();
+}
+
+template<typename T> 
+T fromString(const std::string& s) 
+{
+  istringstream iss(s);
+  T res;
+  iss >> res;
+  return res;
+}
+
+class Cache
+{   
 public:
     
-    Cacher(std::string request, string asnwer, string cachedTime = 0)
-    {    
-        _cacheData._request = request;
-        _cacheData._time = cachedTime;
-        _cacheData._answer = asnwer;        
-    }
+    Cache(const char* cacheFolder, int cacheSize, int timeIntervalInMin);
     
-    void AddToCache()
-    {
-        ofstream casheFile;
-        casheFile.open(casherFolder,ios::app);
-        string finalStr = "{\n" + _cacheData._request + "\n" + _cacheData._time + "\n" + _cacheData._answer + "\n}\n";
-        casheFile << finalStr;
-        casheFile.close();
-    }
+    void Put(string request, string answer);
     
-    cacheInfo * findAllStructs ()
-    {
-        string str;
-	ifstream casheFile;
-	casheFile.open (casherFolder);
-        
-        while(!casheFile.eof)
-        {
-            getline(casheFile,str);
-        }
-        
-        casheFile.close (casherFolder);
-        
-        int structsCount = 0;
-        
-        for (size_t pos = 0; pos < str.size(); ++pos )
-        {
-          pos = str.find('{', pos);
-          if (pos != std::string::npos)
-          {
-            ++structsCount;
-          }
-          else
-          {
-            break;
-          }
-        }
-        
-        cacheInfo * infoStructs = new cacheInfo[structsCount];
-                
-        for(size_t pos = 0; pos < str.size(); ++pos)
-        {
-            pos = str.find('\n', pos);
-            if (pos != std::string::npos)
-            {
-              ++structsCount;
-            }
-            else
-            {
-              break;
-            }
-        }
-        
-        
-//        size_t symbolPos = 0;
-//        size_t symbolPos1 = 0;
-//        //Заполнение массива структур
-//        for(size_t i = 0; i < structsCount; ++i)
-//        {
-//            symbolPos = str.find('{\n', symbolPos);
-//            symbolPos1 = str.find('\n', symbolPos1);
-//            for(symbolPos != str.find('}', symbolPos); ++symbolPos)
-//            {
-//                infoStructs[i]._request = 
-//                infoStructs[i]._time    = 
-//                infoStructs[i]._answer  =
-//            };
-//        };
-
-        return ;
-        
-    }
+    string Get(string request);
     
-    bool IsInCache()
-    {
-        cacheInfo* dataStructsArray  = findAllStructs ();
-        return 0;
-    }
+    void DeleteExpiredCachedData();
     
+    bool IsTimeExpired();
+    
+private:
+    
+    bool IsInCache(string request);
+    bool IsCacheFull();
+    
+    vector <CacheInfo>  GetFullCachedData();
+        
+    const char* _cacheFolder;    
+    int         _cacheSize;
+    int         _timeIntervalInMin;    
 };
-
-
-
-*/
-
-
-//
-//
-//
-//
-//
-//// Stores the cache results, can be inherited from or held as a member.
-//class ResultCache
-//{
-//public:
-//  template <class T> void AddToCache(const T & value, const std::string & name)
-//  {
-//    // Search if it is there
-//    for (std::shared_ptr<CacheInterface> & cache : _cached_items)
-//    {
-//      if (cache->GetName() == name)
-//      {
-//        shared_ptr<CachedValue<T>> cache_value (boost::reinterpret_pointer_cast<CachedValue<T>>(cache));
-//        cache_value->_value = value;
-//        return;
-//      }
-//    }
-//
-//    // Make a new cache value and add it
-//    shared_ptr<CacheInterface> cached_item(new CachedValue<T>(value, name));
-//    _cached_items.push_back(cached_item);
-//  }
-//
-//  // Returns true if cache exists for a name
-//  bool IsInCache(const std::string & name)
-//  {
-//    for (shared_ptr<CacheInterface> & cache : _cached_items)
-//    {
-//      if (cache->GetName() == name)
-//      {
-//        return true;
-//      }
-//    }
-//    return false;
-//  }
-//
-//  // Reads a value from the cache
-//  template <class T> void ReadFromCache(T & output, const std::string & name)
-//  {
-//    for (shared_ptr<CacheInterface> & cache : _cached_items)
-//    {
-//      if (cache->GetName() == name)
-//      {
-//        // Is reinterpret cast safe?
-//        shared_ptr<CachedValue<T>> cache_value (boost::reinterpret_pointer_cast<CachedValue<T>>(cache));
-//        output = cache_value->_value;
-//      }
-//    }
-//  }
-
-  // Clears the cache values
-//  void ClearCache()
-//  {
-//    _cached_items.clear();
-//  }
-//
-//  // Clear cache on copy and assign?
-//
-//
-//private:
-//  vector<int> _cached_items;
-//};
-//
-//// Macros simplified use in code.
-//#define IS_IN_CACHE(x) IsInCache(#x)
-//#define READ_FROM_CACHE(x) ReadFromCache(x, #x)
-//#define CACHE_RESULT(x) AddToCache(x, #x)
-
-
-
-#endif /* CACHER_H */

a548681e8a33c08edf2b594a7869ce76a0fce7fb
Ivandemian <cr0pssp0t@gmail.com>
2017-12-06 00:52:48 -0800

    Удалил метод ExpiredDataCache.Поправил документацию.

--- ProxyServer.Modules/Cacher/Cacher.cpp
+++ ProxyServer.Modules/Cacher/Cacher.cpp
@@ -6,9 +6,11 @@ Cacher::Cacher(const char* cacheFolder, int cacheSize, int timeIntervalInMin)
     _cacheSize   = cacheSize;
     _timeIntervalInMin = timeIntervalInMin;
     
+    //Проверка на созданность.
     if(std::ifstream(cacheFolder).good())
         return;
     
+    //Создаем директорию.
     if(mkdir(cacheFolder, 0777))
     {
         perror("Не удалось создать директорию для кэша");
@@ -18,6 +20,7 @@ Cacher::Cacher(const char* cacheFolder, int cacheSize, int timeIntervalInMin)
 
 Cacher::~Cacher()
 {
+    // Производим удаление закешированных данных.
     for(std::list<CacheInfo>::iterator it = _data.begin(); it != _data.end(); ++it)
     {
         DeleteNote(it--);
@@ -25,38 +28,48 @@ Cacher::~Cacher()
     }
 }
 
-//Add info to cache.
 void Cacher::Put(const std::string& request, const std::string& answer)
 {    
+       
     std::list<CacheInfo>::iterator pos = FindInCache(request);
     
+    //Если найдено в кеше, то отчистить объект листа.
     if(pos != _data.end())
         DeleteNote(pos);
 
+    // Если кеш переполнен, удалить самую ранню запись.
     if(IsCacheFull())
         DeleteEarlyNote();
     
     CacheInfo note;
 
+    //Заполняем объект листа данным
     note.Request = request;
+      
+    // Генерируем имя файла для ответа.
     note.AnswerFile = GenerateNewFileName();
     note.Time = time(NULL);
-            
+    
     _data.push_back(note);
     
+    //Создаем и записываем ответ в файл.
     WriteToFile((_cacheFolder + note.AnswerFile).c_str(), answer);
 }
 
-//Loading data from cache.
 std::string Cacher::Get(const std::string& request)
 {
+    //
     std::list<CacheInfo>::iterator pos = FindInCache(request);
     
+    //Если ничего не найдено по заданному запросу
     if(pos == _data.end())
+        
+        // Выводим пустую строку, обрабатывается далее.
         return "";
     
     std::string filename = pos->AnswerFile;
     
+    //Считываем информацию из файла.
     return ReadFromFile((_cacheFolder + filename).c_str());
 }
 
@@ -72,11 +85,12 @@ std::string Cacher::ReadFromFile(const char* filename)
         exit(2);
     }
     
-    // http://insanecoding.blogspot.ru/2011/11/how-to-read-in-file-in-c.html
+    // Запись данных в результат
     std::string result((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
     
     in.close();
     
+    //Возвращаем ответ.
     return result;
 }
 
@@ -100,21 +114,26 @@ void Cacher::WriteToFile(const char* filename, const std::string& data)
 
 std::string Cacher::GenerateNewFileName()
 {
-    bool success = false;
-    std::string name;
+    
+    bool success = false; // информация о существовании данного имени.
+    std::string name; // Имя файла.
     int j = 0;
     
+    
+    // Формирование имени файла.
     while(success == false)
     {
         time_t currentTime = time(NULL);
         success = true;
         
+        //Генерируем имя по заданному алгоритму.
         for(int i = 20; i < 50; ++i)
         {
             char temp = 'A' + (currentTime % (i + j));
             name += temp;
         }
         
+        //Проверяем на имя на эксклюзивность.
         for(std::list<CacheInfo>::iterator it = _data.begin(); it != _data.end() && success == true; ++it)
         {
             if(it->AnswerFile == name)
@@ -125,49 +144,61 @@ std::string Cacher::GenerateNewFileName()
         }
     }
     
+    // Возвращаем имя файла.
     return name;
 }
 
 std::list<CacheInfo>::iterator Cacher::FindInCache(const std::string& request)
 {
-    time_t currentTime = time(NULL); 
+    time_t currentTime = time(NULL); // Текущее время.
     
+    //Поиск данных в листе.
     for(std::list<CacheInfo>::iterator it = _data.begin(); it != _data.end(); ++it)
     {
+        
+        //Проверка на время хранения в кеше
         if(currentTime - it->Time > _timeIntervalInMin * 60)
         {
             DeleteNote(it--);
             continue;
         }
         
+        //Проверка на совпадение.
         if(it->Request == request)
+            
+            //Возвращаем итератор на найденный объект листа.
             return it;
     }
     
+    //Возвращаем фиктивный итератор, если не найдено.
     return _data.end();
 }
 
 bool Cacher::IsCacheFull()
 {
+    //Проверка на размер кеша.
     return _data.size() == _cacheSize;
 }
 
 void Cacher::DeleteNote(std::list<CacheInfo>::iterator pos)
 {
-    std::string file = pos->AnswerFile;
     
+    std::string file = pos->AnswerFile; // Имя файла с ответом.
+    
+    // Удаление объекта листа.
     _data.erase(pos);
     
+    // Удаление файла с закешированной информацией.
     remove((_cacheFolder + file).c_str());
 }
     
 void Cacher::DeleteEarlyNote()
 {
-    // Перебором удалить самую давнюю запись
-    
+      
     std::list<CacheInfo>::iterator minIt = _data.begin();
     time_t minTime = minIt->Time;
 
+    //Находим самую ранню запись перебором.
     for(std::list<CacheInfo>::iterator it = _data.begin(); it != _data.end(); ++it)
     {
         if(minTime > it->Time)
@@ -177,5 +208,6 @@ void Cacher::DeleteEarlyNote()
         }
     }
     
+    // Удаляем самую ранню запись.
     DeleteNote(minIt);
 }
--- ProxyServer.Modules/Cacher/Cacher.h
+++ ProxyServer.Modules/Cacher/Cacher.h
@@ -10,37 +10,93 @@
 #ifndef CACHER_H
 #define CACHER_H
 
+// Информация о кеширующихся данных.
 struct CacheInfo
 {
     std::string Request;
     std::string AnswerFile;
     int Time;	
 };
-
+/**
+ * Класс для работы с кеширумой информацией.
+ */
 class Cacher
 {   
 public:
-    
+    /**
+     * 
+     * @param cacheFolder           Директория для записи ответов.
+     * @param cacheSize             Размер кеша.
+     * @param timeIntervalInMin     Время нахождения актуальной информации в кеше.
+     */
     Cacher(const char* cacheFolder, int cacheSize, int timeIntervalInMin);
     ~Cacher();
+    
+    /**
+     * Кладем информацию в кеш.
+     * @param request Запрос.
+     * @param answer  Ответ.
+     */
     void Put(const std::string& request, const std::string& answer);
+    
+    /**
+     * Достаем информацию из кеша.
+     * @param request Запрос.
+     * @return        Возвращает ответ на заданный вопрос или пустую строку.
+     */
     std::string Get(const std::string& request);
-    void DeleteExpiredCachedData();
     
 private:
+    
+    /**
+     * Поиск информации в кеше.
+     * @param request Запрос.
+     * @return        Возвращает итератор на объект листа, с заданным запросом.
+     */
     std::list<CacheInfo>::iterator FindInCache(const std::string& request);
+    
+    /**
+     * Проверка на заполненность кеша.
+     * @return 
+     */
     bool IsCacheFull();
     
+    /**
+     * Считываем ответ из файла.
+     * @param filename имя файла.
+     * @return         возвращет прочитанный ответ из файла.
+     */
     std::string ReadFromFile(const char* filename);
+    
+    /**
+     * Записываем ответ в файл.
+     * @param filename
+     * @param data
+     */
     void WriteToFile(const char* filename, const std::string& data);
+    
+    /**
+     * Генерируем имя файла. 
+     * @return Возвращает имя файла.
+     */
     std::string GenerateNewFileName();
     
+    /**
+     * Удаляем информацию из кеша.
+     * @param pos итератор на объект класса.
+     */
     void DeleteNote(std::list<CacheInfo>::iterator pos);
+    
+    /**
+     * Удаляем самую ранню запись.
+     */
     void DeleteEarlyNote();
         
-    const char* _cacheFolder;    
-    int         _cacheSize;
-    int         _timeIntervalInMin;   
+    const char* _cacheFolder;        // Имя директории   
+    int         _cacheSize;          // Размер кеша.
+    int         _timeIntervalInMin;  // Время хранения кеша.
+    
+    //Лист для хранения информации о кеше.
     std::list<CacheInfo> _data;
 };
 
